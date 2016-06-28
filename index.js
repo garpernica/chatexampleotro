3640d30512a381f979d3cff9bfce167f4305ee4e
Oscar Valdivia <catric23@gmail.com>
2016-06-28 14:34:43 +0800

    change server

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var path = require('path');
+
+
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(path.resolve('index.html'));
 });
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
